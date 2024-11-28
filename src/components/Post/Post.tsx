9207a01f2f90f7f76cf2697a327e473928a84783
Luca Di Pietro <luca.dipietro@relatech.com>
2024-11-28 17:49:12 +0100

    fix form with 2way bind and set post state in post component

--- src/components/Post/Post.tsx
+++ src/components/Post/Post.tsx
@@ -1,19 +1,44 @@
-import {FC} from "react";
+import {ChangeEvent, FC, useState} from "react";
 import IPost from "../../models/IPost.ts";
-import Input from "../Input/Input.tsx";
 
 type Props = {
     post: IPost
 }
 
 const Post: FC<Props> = ({post}) => {
+    const [userPost, setPost] = useState<IPost>(post)
 
-    return(
+    function handleInputChange(event: ChangeEvent<HTMLInputElement>): void {
+        const value = event.target.value;
+        const inputId = event.target.id
+        if (inputId === 'title') {
+            setPost(prevPost => {
+                return {
+                    ...prevPost,
+                    title: value
+                }
+            })
+        } else if (inputId === 'body') {
+            setPost(prevPost => {
+                return {
+                    ...prevPost,
+                    body: value
+                }
+            })
+        }
+    }
+
+    return (
         <>
             <form>
-                {/*<h4>TITLE</h4>*/}
-                <Input label='TITLE' type='text' value={post.title}/>
-                <Input label='BODY' type='text' value={post.body}/>
+                <div>
+                    <label htmlFor='title'>TITLE</label>
+                    <input type='text' id='title' name='title' value={userPost.title} onChange={handleInputChange}/>
+                </div>
+                <div>
+                    <label htmlFor='body'>BODY</label>
+                    <input type='text' id='body' name='body' value={userPost.body} onChange={handleInputChange}/>
+                </div>
             </form>
         </>
     )
